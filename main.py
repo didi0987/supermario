2b5af4e12ae0e7c16f1b6fdbceb94d2781156880
chenxiaowang <didi0987@gmail.com>
2023-05-29 01:54:40 -0500

    add more stuff

--- main.py
+++ main.py
@@ -2,15 +2,35 @@ import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
-
+from gym.wrappers import GrayScaleObservation
+from stable_baselines3.common.vec_env import DummyVecEnv,VecFrameStack
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.logger import configure
+from stable_baselines3.common.callbacks import BaseCallback
+import os 
+# Import PPO for algos
+from stable_baselines3 import PPO
+# Import Base Callback for saving models
+from stable_baselines3.common.callbacks import BaseCallback
+LOG_DIR='./Logs'
 env=gym_super_mario_bros.make('SuperMarioBros-v0')
 env=JoypadSpace(env,SIMPLE_MOVEMENT)
-
+env = GrayScaleObservation(env, keep_dim=True)
+# 4. Wrap inside the Dummy Environment
+env = DummyVecEnv([lambda: env])
+# 5. Stack the frames
+env = VecFrameStack(env, 4, channels_order='last')
+model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR,verbose=1,learning_rate=0.000001, 
+            n_steps=512) 
+# Train the AI model, this is where the AI model starts to learn
+model.learn(total_timesteps=10000, log_interval=1)
+model.save('testmodel2')
+'''
 done=True
 for step in range(15000):
     if done:
         state=env.reset()
     state, reward, done, info=env.step(env.action_space.sample())
     env.render()
-env.close()
+env.close()'''
 
--- test.py
+++ test.py
@@ -1,10 +1,33 @@
-import gymnasium as gym
-env=gym.make("LunarLander-v2",render_mode="human")
-observation, info=env.reset()
-for _ in range (1000):
-    action=env.action_space.sample()
-    observation, reward, terminated, truncated, info= env.step(action)
-    if terminated or truncated:
-        observation=env.reset()
-env.close()
+import gym_super_mario_bros
+from stable_baselines3.common.vec_env import DummyVecEnv,VecFrameStack
+from gym.wrappers import GrayScaleObservation
+from nes_py.wrappers import JoypadSpace
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from gym_super_mario_bros.actions import RIGHT_ONLY
+from stable_baselines3 import PPO
+from random import *
+if __name__=="__main__":
+    model=PPO.load('testmodel1')
+    env=gym_super_mario_bros.make('SuperMarioBros-v0')
+    env=JoypadSpace(env,SIMPLE_MOVEMENT)
+    env = GrayScaleObservation(env, keep_dim=True)
+# 4. Wrap inside the Dummy Environment
+    env = DummyVecEnv([lambda: env])
+# 5. Stack the frames
+    env = VecFrameStack(env, 4, channels_order='last')
+    '''
+    state=env.reset()
+    for _ in range(1500):
+        env.step([randint(1, 150)])
+        env.render()
+    '''
+    state=env.reset()    
+    while True:
+        action,state=model.predict(state)
+        #state,reward,done,info= env.step([env.action_space.sample()])        
+        state,reward,done,info= env.step(action)        
+        print(f"Pos: {info[0]['x_pos']},{info[0]['y_pos']}")
+        print(f"Action: {SIMPLE_MOVEMENT[action[0]]}")
+        env.render()
+    env.close()
 
