2e8d09649cf5155ea78ac08c50645ecf71b77a1d
didi0987 <didi0987@gmail.com>
2023-06-02 06:50:06 -0500

    reduce training size

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ env = VecFrameStack(env, 4, channels_order='last')
 model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR,verbose=1,learning_rate=0.000001, 
             n_steps=512) 
 # Train the AI model, this is where the AI model starts to learn
-model.learn(total_timesteps=200000)
+model.learn(total_timesteps=50000)
 model.save('testmodel3')
 '''
 done=True
