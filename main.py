c149fab07ad3b6b08726c89eeffb98b9b87c3241
didi0987 <didi0987@gmail.com>
2023-06-02 06:29:37 -0500

    add save log

--- main.py
+++ main.py
@@ -13,6 +13,24 @@ from stable_baselines3 import PPO
 # Import Base Callback for saving models
 from stable_baselines3.common.callbacks import BaseCallback
 LOG_DIR='./Logs'
+CHECKPOINT_DIR = './train'
+class TrainAndLoggingCallback(BaseCallback):
+
+    def __init__(self, check_freq, save_path, verbose=1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
+            self.model.save(model_path)
+
+        return True
 env=gym_super_mario_bros.make('SuperMarioBros-v3')
 env=JoypadSpace(env,SIMPLE_MOVEMENT)
 env = GrayScaleObservation(env, keep_dim=True)
@@ -20,11 +38,11 @@ env = GrayScaleObservation(env, keep_dim=True)
 env = DummyVecEnv([lambda: env])
 # 5. Stack the frames
 env = VecFrameStack(env, 4, channels_order='last')
-model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR,verbose=1,learning_rate=0.000001, 
-            n_steps=512) 
+callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
+model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR,verbose=1,learning_rate=0.000001, n_steps=512) 
 # Train the AI model, this is where the AI model starts to learn
-model.learn(total_timesteps=10000)
-model.save('testmodel2')
+model.learn(total_timesteps=1000000,callback=callback)
+#model.save('testmodel2')
 '''
 done=True
 for step in range(15000):
